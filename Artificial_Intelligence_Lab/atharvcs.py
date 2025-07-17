4e877c852137822226f0c27ccef351f24380715b
atharva <atharva.gade24@mmit.edu.in>
2025-07-17 23:47:22 +0530

    atharvcs.py

---
+++ Artificial_Intelligence_Lab/atharvcs.py
@@ -0,0 +1,8 @@
+
+    print("course is computer science engineering")
+elif ((sub1==3 and sub2==1) or (sub1==1 and sub2==3)):
+    print("course is electronics engineering")
+elif ((sub1==7 and sub2==6) or (sub1==6 and sub2==7)):
+    print("course is artifical intelligence data science")
+elif ((sub1==7 and sub2==8) or (sub1==8 and sub2==7)):
+    print("course is ai and ml engineering")
